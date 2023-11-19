bb39428e7f35a37fd26f9fa4f0c1ed39e4323fe4
Utraclown <201220024@smail.nju.edu.cn>
2023-11-19 23:32:08 +0800

    update

--- TCP/TCP_Server/server.h
+++ TCP/TCP_Server/server.h
@@ -3,6 +3,12 @@
 #else
 # include <sys/socket.h>
 #endif
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#pragma warning(disable : 4996)
+#pragma comment(lib, "ws2_32.lib")
+#define USER_ERROR -1
 
 class Server
 {
